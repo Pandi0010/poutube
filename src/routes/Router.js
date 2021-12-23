47a3afd87d1532d532afa3fe515ec4bcba2aa63e
Pandi <pandi@wavetogether.com>
2021-12-23 15:11:54 +0900

    setting

---
+++ src/routes/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+export default AppRouter;
