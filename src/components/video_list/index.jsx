4ec7b3fab5534c9c04b6a6c50eb195f623cb7456
Pandi <pandi@wavetogether.com>
2021-12-28 17:27:27 +0900

    검색된 영상 나열 및 CSS

--- src/components/video_list/index.jsx
+++ src/components/video_list/index.jsx
@@ -3,6 +3,7 @@ import VideoItem from "../video_item";
 import styles from "./styles.css";
 
 const VideoList = ({ videos, onVideoClick, display }) => {
+  //검색된  영상 리스트 나열
   return (
     <ul className={styles.videos}>
       {videos.map((video) => (
